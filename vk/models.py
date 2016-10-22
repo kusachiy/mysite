8097c461dac400929ec887ef7e2cba423032c5bc
Kusachiy <xavierernandez@gmail.com>
2016-10-23 00:19:51 +0300

    Fix commit11

--- vk/models.py
+++ vk/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
-    id = models.AutoField(primary_key=True)
-    wall_id = models.IntegerField()
-    body = models.TextField()
-    author = models.ForeignKey(Person)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
@@ -21,6 +13,14 @@ class Person(models.Model):
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    wall_id = models.IntegerField()
+    body = models.TextField()
+    author = models.ForeignKey(Person)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 
 class Friends(models.Model):
