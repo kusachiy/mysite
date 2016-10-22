144c9d8f88b5104e1135bfc4809bc5a76363e3f3
Kusachiy <xavierernandez@gmail.com>
2016-10-22 23:22:53 +0300

    Fix commit5

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'),
-            '/var/www/media/',)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
